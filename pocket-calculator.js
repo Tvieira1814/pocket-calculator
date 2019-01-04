627cbce17cd0d941e7e1345828c5a76f40317513
Thomas <Thomasv1814@gmail.com>
2019-01-04 10:45:29 -0500

    dub2

--- pocket-calculator.js
+++ pocket-calculator.js
@@ -1,5 +1,5 @@
 var expression="";
-function fourfunction(operation) {
+function fourfunction (operation) {
   if (operation == '+') {
    expression+="+";
 }
@@ -13,16 +13,63 @@ if (operation=='-') {
   expression+='-';
 }
 
-console.log(expression);
+console.log (expression);
 
 }
-function numbers(operation) {
-  if (operation=='.') {
-    expression+='.';
+function numbers (operation) {
+if (operation=='.') {
+  expression+='.';
 }
 if (operation=='0') {
   expression+='0';
 }
 if (operation=='1') {
   expression+='1';
+}
+if (operation=='2') {
+  expression+='2';
+}
+if (operation=='3') {
+  expression+='3';
+}
+if (operation=='4') {
+  expression+='4';
+}
+if (operation=='5') {
+  expression+='5'
+}
+if (operation=='6') {
+  expression+='6';
+}
+if (operation=='7') {
+  expression+='7';
+}
+if (operation=='8') {
+  expression+='8';
+}
+if (operation=='9') {
+  expression+='9';
+}
+console.log (expression);
+}
+function equals (operation) {
+
+
+  if (operation== '=') {
+    if (eval(expression)==Infinity) {
+      document.getElementById('addition').innerHTML="Error";
+    }
+    else{
+
+  document.getElementById('addition').innerHTML=eval(expression):
+    }
+
+}
+
+  try {
+    eval(expression)
+  }
+  catch(err) {
+    document.getElementById('adddition').innerHTML="Error";
+  }
 }
